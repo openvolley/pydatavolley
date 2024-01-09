8b14a31739c6945be4603b6242dd7e0f501adab2
tyler-widdison <tylerperrywiddison@gmail.com>
2024-01-09 02:13:33 -0700

    Built site for gh-pages

--- venv/Lib/site-packages/janitor/errors.py
+++
@@ -1,2 +0,0 @@
-class JanitorError(Exception):
-    pass
--- venv/Lib/site-packages/multipledispatch/__init__.py
+++
@@ -1,9 +0,0 @@
-from .core import dispatch
-from .dispatcher import (
-    Dispatcher,
-    halt_ordering,
-    restart_ordering,
-    MDNotImplementedError,
-)
-
-__version__ = "0.6.0"
